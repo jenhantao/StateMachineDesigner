cdbb8d8ea3948cbae44037847dff2dd684ac14fd
jenhan <jenhan@jenhan-M-6888u.(none)>
2011-07-22 21:39:49 -0400

    first commit

---
+++ src/statemachinedesigner/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package statemachinedesigner;
+
+/**
+ *
+ * @author Henry
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    SimulatorFrame sf = new SimulatorFrame();
+    sf.pack();
+    sf.setVisible(true);
+    }
+
+}
