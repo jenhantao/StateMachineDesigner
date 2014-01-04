4ef4bb66d86e7d433b2bfb5e6d5c572bc08bca79
Jenhan Tao <jenhantao@gmail.com>
2014-01-04 02:42:07 -0800

    shit changed

--- stateMachineDesigner.py
+++ stateMachineDesigner.py
@@ -19,28 +19,23 @@ for line in input:
 root = Node(); # root has no letters because there are multiple starting letters
 
 # add words to graph
-while len(words) > 0:
-	word = words[0];
+for i in range(len(words)):
 	currentNode = root;
-
-	for letter in list(word):
-		needNew = True;
-		if len(currentNode.children) > 0:
-			count = 0;
-			for child in currentNode.children:
-				count = count + 1;
-				if child.letter == letter:
-					currentNode = child;
-					needNew = False;
-				if count == len(currentNode.children):
-					needNew = True		
-				print("count: "+str(count) + " length: " + str(len(currentNode.children))+ " letter: "+str(currentNode.letter))
-		else:
-			needNew = True;
-		print needNew
-		if needNew:
-			newNode = Node(letter);
-			currentNode.children.append(newNode);
+	letterArray = list(words[i]);
+	previousLetter = "*";
+	print("current word: "+words[i]);
+	for j in range(len(letterArray)):
+		print("current letter: "+ str(j)+" "+letterArray[j]);
+		createNew = True;
+		currentLetter = letterArray[j];
+		for child in currentNode.children:
+			if child.letter == currentLetter and not previousLetter== currentLetter:
+				currentNode = child;
+				createNew = False;
+				break;
+		if createNew:
+			newNode = Node(currentLetter);
+			currentNode.children.append(newNode)
 			currentNode = newNode;
-	if len(words) > 0:
-		del words[0]
+			print "adding new node"
+		previousLetter = currentLetter
