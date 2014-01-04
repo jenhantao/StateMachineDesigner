d9a8051bfb2d1e81d2b50d1645596e1808bda314
Jenhan Tao <jenhantao@gmail.com>
2014-01-04 05:31:05 -0800

    graph building works

--- stateMachineDesigner.py
+++ stateMachineDesigner.py
@@ -1,41 +1,53 @@
 ### Imports ###
-import sys;
+import sys
 
 ### Classes ###
 class Node:
-	children = [];
-	def __init__(self,aLetter = None):
-		self.letter = aLetter;
+	def __init__(self,aLetter = None,aLevel=-1):
+		self.letter = aLetter
+		self.level = aLevel
+		self.children = []
 ### Arguments ###
-inputFileName = sys.argv[1];
+inputFileName = sys.argv[1]
 
-inputFile = open(inputFileName);
-input = inputFile.readlines();
+inputFile = open(inputFileName)
+input = inputFile.readlines()
 
 # generate graph
-words = [];
+words = []
 for line in input:
-	words.append(line.strip());
-root = Node(); # root has no letters because there are multiple starting letters
+	words.append(line.strip())
+root = Node() # root has no letters because there are multiple starting letters
 
 # add words to graph
 for i in range(len(words)):
-	currentNode = root;
-	letterArray = list(words[i]);
-	previousLetter = "*";
-	print("current word: "+words[i]);
+	currentNode = root
+	letterArray = list(words[i])
+	previousLetter = "*"
+	#print("current word: "+words[i])
 	for j in range(len(letterArray)):
-		print("current letter: "+ str(j)+" "+letterArray[j]);
-		createNew = True;
-		currentLetter = letterArray[j];
+		#print("current letter: "+ str(j)+" "+letterArray[j])
+		createNew = True
+		currentLetter = letterArray[j]
 		for child in currentNode.children:
-			if child.letter == currentLetter and not previousLetter== currentLetter:
-				currentNode = child;
-				createNew = False;
-				break;
+			if child.letter == currentLetter and child.level ==j:
+				currentNode = child
+				createNew = False
+				break
 		if createNew:
-			newNode = Node(currentLetter);
+			newNode = Node(currentLetter,currentNode.level+1)
 			currentNode.children.append(newNode)
-			currentNode = newNode;
-			print "adding new node"
+			currentNode = newNode
+			#print "******** adding new node ********"
 		previousLetter = currentLetter
+def printGraph(root):
+	queue = []
+	queue.append(root)
+	while queue:
+		currentNode = queue[0]
+		queue.remove(currentNode)
+		print(str(currentNode.level)+" "+str(currentNode.letter))
+		for child in currentNode.children:
+			queue.append(child)
+printGraph(root)
+
